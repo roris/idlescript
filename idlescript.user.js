c1cf032ec71c518629ad9152bb84d170c25e67a4
roris <roris@users.noreply.github.com>
2015-10-16 02:06:48 +0500

    Fix afk messages not being sent
    Also use a text input instead of a text area.

--- idlescript.user.js
+++ idlescript.user.js
@@ -3,7 +3,7 @@
 // @namespace   idlescript
 // @description Prevent Gikopoi timeouts.
 // @include     http://l4cs.jpn.org/gikopoi/flash/gikopoi*/flash_gikopoi.html
-// @version     1.6.0
+// @version     1.6.1
 // @grant       none
 // @updateURL   https://github.com/roris/idlescript/raw/master/idlescript.user.js
 // ==/UserScript==
@@ -12,7 +12,6 @@
     return new Date().getTime() / 1000;
   }
   var keepAliveInterval = 1500; // keep alive interval in seconds
-  var intervalFn = undefined;
   var afk = false;
   var prevTime = now();
   var btnAfk = doc.createElement('button');
@@ -20,7 +19,7 @@
   var btnList = doc.createElement('button');
   var divTimer = doc.createElement('div');
   var divPanel = doc.createElement('div');
-  var txtAfk = doc.createElement('textarea');
+  var txtAfk = doc.createElement('input');
   function sendMessage(m) {
     doc['gikopoi'].JSCallBackSendMessage(m);
   }
@@ -31,9 +30,9 @@
     var tDelta = now() - prevTime;
     var deltaS = Math.floor(tDelta % 60);
     var deltaM = Math.floor(25 - (tDelta / 60));
-    deltaS = deltaS == 0 ? 0 : 60 - deltaS;
+    deltaS = (deltaS == 0 ? 0 : 60 - deltaS);
     if (tDelta >= keepAliveInterval) {
-      sendMessage(afk ? textAfk.content : '');
+      sendMessage(afk ? txtAfk.value : '');
       divTimer.style.color = 'black';
       prevTime = now();
       deltaS = 0;
@@ -45,8 +44,7 @@
   }
   function startKeepAlive() {
     prevTime = now();
-    sendKeepAlive();
-    intervalFn = setInterval(sendKeepAlive, 1000);
+    setInterval(sendKeepAlive, 1000);
   }
   btnRula.textContent = 'Rula';
   btnRula.onclick = function () {
@@ -61,8 +59,7 @@
     afk = !afk;
     btnAfk.style.color = afk ? 'red' : 'black';
     if (afk) {
-      clearInterval(intervalFn);
-      startKeepAlive();
+      sendMessage(txtAfk.value);
     }
   };
   divTimer.style.color = 'black';
@@ -72,12 +69,11 @@
   divPanel.style.position = 'fixed';
   divPanel.style.top = 0;
   divPanel.style.right = 0;
+  txtAfk.type = 'text';
+  txtAfk.size = 16;
   txtAfk.style.top = 48;
   txtAfk.style.right = 0;
   txtAfk.style.position = 'fixed';
-  txtAfk.rows = 5;
-  txtAfk.cols = 32;
-  txtAfk.style.resize = 'none';
   divPanel.appendChild(btnAfk);
   divPanel.appendChild(btnRula);
   divPanel.appendChild(btnList);
